685b5e7c38791c41a8e5a913c244f40d927da1af
Daniel Chong <danielgq.chong@gmail.com>
2018-05-11 22:31:51 +1200

    added slackrequest types

--- out/typed-slack.d.ts
+++ out/typed-slack.d.ts
@@ -180,7 +180,15 @@ export interface SlackInterActiveMessage {
     trigger_id: string;
 }
 export interface SlackRequest {
+    token: string;
     team_id: string;
+    team_domain: string;
+    channel_id: string;
+    channel_name: string;
     user_id: string;
+    user_name: string;
+    command: string;
     text: string;
+    response_url: string;
+    trigger_id: string;
 }
--- src/typed-slack.ts
+++ src/typed-slack.ts
@@ -174,7 +174,15 @@ export interface SlackInterActiveMessage {
 }
 
 export interface SlackRequest {
+  token: string
   team_id: string
+  team_domain: string
+  channel_id: string
+  channel_name: string
   user_id: string
+  user_name: string
+  command: string
   text: string
+  response_url: string
+  trigger_id: string
 }
